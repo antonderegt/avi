2619efe3f0162dc8aba536e33c2139381b158db2
Anton <antonderegt@pm.me>
2020-07-21 21:14:07 +0200

    Add highlight for #define and #include

--- kilo.c
+++ kilo.c
@@ -94,7 +94,7 @@ char *C_HL_extensions[] = {".c", ".h", ".cpp", NULL};
 char *C_HL_keywords[] = {"switch",    "if",      "while",   "for",    "break",
                          "continue",  "return",  "else",    "struct", "union",
                          "typedef",   "static",  "enum",    "class",  "case",
-                         "int|",      "long|",   "double|", "float|", "char|",
+                         "#define", "#include", "int|",      "long|",   "double|", "float|", "char|",
                          "unsigned|", "signed|", "void|",   NULL};
 
 char *MAKE_HL_extensions[] = {"Makefile", NULL};
